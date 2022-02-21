31baa2caa46b50f3be46de9f5536b48ac378268a
Ione Fine <ionefine@uw.edu>
2022-02-21 10:55:09 -0800

    new version
    main_currentspread is still the main file. cs. contains methods

---
+++ cs.m
@@ -0,0 +1,105 @@
+% cs.m
+%
+% holds all support functions for current spread
+% project.
+%
+% functions can be called from outside with 'cs.<function name>'
+
+
+classdef cs
+    methods(Static)
+        function ret = calc_dist_from_electrode(ret)
+            for i = 1:length(ret.x)
+                ret.e(i).R = sqrt(((ret.X-ret.x(i)).^2) + ((ret.Y-ret.y(i)).^2)+((ret.Z-ret.z).^2));
+                ret.e(i).R = ret.e(i).R-ret.rad;
+                ret.e(i).R(ret.e(i).R<0) = 0;
+                ret.e(i).R = ret.e(i).R./(max(ret.e(i).R(:))); % deal with directly under the electrode
+            end
+        end
+        function ret = create_currentspread(ret)
+            % takes in parameter values describing electrodes and
+            % calculates the resulting current field
+
+            ret.I = zeros(size(ret.e(1).R));
+            for i = 1:length(ret.x) % for each electrode
+                ret.e(i).I = ret.t./(1+((ret.k.*ret.e(i).R).^ret.a));
+                ret.I = ret.I + ret.e(i).I;
+            end
+            ret.I(ret.Z<max([ret.z ret.z])) = 0; % remove all current above the electrode
+            if isfield(ret, 'loc') % if choosing a particular location on the retina at which to know amplitude
+                ret.Vq = interpn(unique(ret.Y), unique(ret.X), unique(ret.Z), ret.I, ret.loc(2), ret.loc(1), ret.loc(3));
+            else % find the max amplitude
+                ret.Vq = max(max(ret.I(:, :, round(size(ret.I, 3)/2))));
+            end
+        end
+
+        function [err] = fit_currentspread(ret)
+            % calculates current spread, and finds the electrode amplitude
+            % which produces a current of ret.t_ret on the retinal surface
+            ret = cs.create_currentspread(ret);
+            err = (ret.Vq(1)-ret.t_ret).^2;
+        end
+
+        function ret = fit_currentspreadfast(ret, fitParams)
+
+            lo = fitParams.lo; hi = fitParams.hi;
+            for i=1:fitParams.nreps
+                ret.t = (hi+lo)/2;
+                ret = cs.create_currentspread(ret);
+                if ret.t_ret < ret.Vq
+                    hi = ret.t;
+                else
+                    lo = ret.t;
+                end
+
+                % if the fitting doesn't seem to work, check that the original hi and
+                % lo ranges aren't too narrow
+            end
+            ret.t = (hi+lo)/2;
+            % disp(['max current on retina = ', num2str(ret.Vq), ' retinal thresh = ', num2str(ret.t_ret)]);
+        end
+
+
+        function I = create_currentspreadfig(ret)
+            Sxy = ret.I(:, :, round(size(ret.I, 3)/2)); % slice on the retina, z = 0
+
+            subplot(1,2,1)
+            imagesc(unique(ret.X(:)), unique(ret.Y(:)), Sxy*ret.Isc); colormap(hot); hold on
+            for i = 1:length(ret.x)
+                h = viscircles([ret.x(i), ret.y(i)],ret.rad-1, 'Color', [.8 .8 .3], 'LineWidth', .5);
+            end
+            if isfield(ret, 'loc')
+                x = unique(ret.X(:)); y = unique(ret.Y(:));
+                for l = 1:size(ret.loc, 1)
+                    plot(x(ret.loc(l, 1)),y(ret.loc(l, 2)),'g*');
+                end
+                plot(0, 0, 'b*')
+            end
+            if isfield(ret, 't_ret')
+                contour(unique(ret.X(:)), unique(ret.Y(:)), Sxy, [-1 ret.t_ret*.95], 'g--');
+            end
+            axis equal
+            title('retinal plane');
+
+            subplot(1,2,2)
+            Sxz = flipud(rot90(squeeze(ret.I(round(size(ret.I, 1)/2), :, :)))); % slice along the midpoint of y
+            imagesc(unique(ret.X(:)), unique(ret.Z(:)),Sxz*ret.Isc); colormap(hot); hold on
+            plot(unique(ret.X(:)), zeros(size(unique(ret.X(:)))), 'w--');
+            for i = 1:length(ret.x)
+                plot([ret.x(i)-ret.rad, ret.x(i)+ret.rad], [ret.z-3, ret.z-3],'-', 'Color', [.8 .8 .3],  'LineWidth', 2);
+            end
+            if isfield(ret, 't_ret')
+                contour(unique(ret.X(:)), unique(ret.Z(:)), Sxz, [-1 ret.t_ret]*.95, 'g--');
+            end
+            title('depth plane');
+
+            axis equal
+            if isfield(ret, 'loc')
+                for l = 1:size(ret.loc, 1)
+                    plot(x(ret.loc(l, 1)), 0, 'g*');
+                end
+                  plot(0, 0, 'b*')
+            end
+        end
+    end
+end
--- main_currentspread.m
+++ main_currentspread.m
@@ -2,166 +2,288 @@
 close  all
 clear all
 
-
+lowres = 1;
 % electrode parameters
-%p.x1 = -2600; p.x2 = 2600; % separation of the electodes, max value
-p.x1 = -1000; p.x2 = 1000;
-p.lim = [abs(p.x1)*1.5, 500, 1100]; % how much retina to simulate
-ss = 25; % simulation resolution
-p.y = 0; p.rad = 225/2;
-p.gamma = 1.69; p.amp_min =  50;
+ret.z_range = 0:25:1000; % different heights (microns)
+
+
+ret.rad = 225/2;
+ret.lim = [500, 1500, 1100]; % how much retina to simulate. x, y, z
+ret.ss = 25; % simulation resolution
 
 % plot params
-p.plot = 1;
-p.pos = [ 42   1088  939   231];
+plt.pos = [ 42   1088  939   231]; 
 
 % how bright the current is on the graphs
-p.Isc = .4;
+ret.Isc = .4;
 
-rd_v = -p.lim(1):ss:p.lim(1);
-z_v = -p.lim(2):ss:p.lim(2);
-zv = -p.lim(3):ss:p.lim(3);
-[p.X,p.Y,p.Z] = meshgrid(rd_v,z_v,zv);
-p.threshOrig = 23.392 *8.5;
+xv = -ret.lim(1):ret.ss:ret.lim(1);
+yv = -ret.lim(2):ret.ss:ret.lim(2);
+zv = -ret.lim(3):ret.ss:ret.lim(3);
+[ret.X,ret.Y,ret.Z] = meshgrid(yv,xv,zv); % simulates the retina as a grid
 
-k_vals = [3:.25:7];% 3::7; % controls current spread
-rd_vals = 0:50:1000; % threshold elevation due to retinal damage
-z_vals = 0:50:1000; % different heights
+% neurophysiological paramers
 
-amp_no_rd = NaN(length(k_vals), length(rd_vals));
+ret.k_range = 6:3:20; % controls current spread
+ret.a_range = 1:.5:3;
+good_ak = [0     1     1     1     1
+     1     1     1     1     1
+     1     1     1     1     1
+     1     1     1     0     0
+     1     1     0     0     0]
 
-%% find thresholds as a function of height, where no retinal damage, single electrode!
-%%single electrode
-for k = 1:length(k_vals)
-    p.k = k_vals(k);
-    for z = 1:length(z_vals)
-        p.z = -z_vals(z);
-        p.R1 = sqrt(((p.X-p.x1).^2) + ((p.Y-p.y).^2)+((p.Z-p.z).^2));
-        p.R1 = p.R1-p.rad; p.R1(p.R1<0) = 0; p.R1 = p.R1./(max(p.R1(:)));
-        p.R2 = sqrt(((p.X-p.x2).^2) + ((p.Y-p.y).^2)+((p.Z-p.z).^2));
-        p.R2 = p.R2-p.rad; p.R2(p.R2<0) = 0; p.R2 = p.R2./(max(p.R2(:)));
-        
-        p.a1 = 400; p.a2 = 0; 
-        p.thresh = p.amp_min; % each electrode goes up to 127
-        % find the threshold for the z
-        [p,~] = fit('fit_currentspread',p, {'a1'});
-        amp_no_rd_single(k, z) = p.a1;
-    end
-    figure(100); 
-    y = amp_no_rd_single(k, :);
-    plot(z_vals, y , 'k'); hold on
-    xlabel('z'); ylabel('threshold non damaged retina  ')
-    text(z_vals(end-1), y(end-1), ['k=',num2str(k_vals(k))]);
-end
+ret.t_ret_min = 50; % minimum retinal current required to reach perceptual
 
+% threshold
+ret.t_rd_range = 0:100:600; % threshold elevation due to retinal damage (mAmps)
 
+fitParams.tol = 0.001;
+fitParams.lo = 0;
+fitParams.hi = 5000;
+fitParams.thr = 1;
+fitParams.nreps = 20;
 
-%% Panel A
-p.k = 5; p.z = -500; rd = 200;
-p.R1 = sqrt(((p.X-p.x1).^2) + ((p.Y-p.y).^2)+((p.Z-p.z).^2));
-p.R1 = p.R1-p.rad; p.R1(p.R1<0) = 0; p.R1 = p.R1./(max(p.R1(:)));
-p.R2 = sqrt(((p.X-p.x2).^2) + ((p.Y-p.y).^2)+((p.Z-p.z).^2));
-p.R2 = p.R2-p.rad; p.R2(p.R2<0) = 0; p.R2 = p.R2./(max(p.R2(:)));
-p.thresh = 400; 
-p.a = 400;p = rmfield(p, 'a1'); p = rmfield(p,'a2');
+%% calculate thresholds as a function of height, where no retinal damage, single electrode
+ret_s = ret; ret_s.t_ret = ret.t_ret_min;
+ret_s.x = 0; ret_s.y = 0;
+ret_s.t = 400; % current on the electrode
 
-[p,err] = fit('fit_currentspread',p, {'a'});
-% now double it and calculate what the current spread looks like
-p.a = p.a*2;
-[I, p, Vq] = create_currentspread(p);
-
-figure(1); clf; p.pos = [ 42   1088   939   231];
-set(gcf, 'Name', ['k = ', num2str(p.k), ' height = ', num2str(p.z), 't = ', num2str(p.thresh)]);
-create_currentspreadfig(I, p); drawnow
-
-        
-%% now calculate surfaces for wide range of retinal damage, no figure
-thresh = NaN(length(k_vals), length(z_vals), length(r_vals));
-amp = thresh; amp_underelectrode = thresh;amp_max = thresh;amp_midpoint = thresh; gv = thresh;
-for k = 1:length(k_vals)
-    p.k = k_vals(k);
-    for z = 1:length(z_vals)
-        p.z = -z_vals(z);
-        p.R1 = sqrt(((p.X-p.x1).^2) + ((p.Y-p.y).^2)+((p.Z-p.z).^2));
-        p.R1 = p.R1-p.rad; p.R1(p.R1<0) = 0; p.R1 = p.R1./(max(p.R1(:)));
-        p.R2 = sqrt(((p.X-p.x2).^2) + ((p.Y-p.y).^2)+((p.Z-p.z).^2));
-        p.R2 = p.R2-p.rad; p.R2(p.R2<0) = 0; p.R2 = p.R2./(max(p.R2(:)));
-        for r = 1:length(rd_vals)
-            p.a = 400;
-            p.thresh = rd_vals(r) +  amp_no_rd(k, z);
-            thresh(k,z,r) = p.thresh;
-            % find the amplitude to reach this threshold
-            [p,err] = fit('fit_currentspread',p, {'a'});
-            amp(k,z,r) = p.a;
-            
-            % now double it and calculate what the current spread looks like
-            p.a = p.a*2;
-            [I, p, Vq] = create_currentspread(p); 
-          
-            % current value directly under the electrode on retinal surface
-            amp_underelectrode(k, z, r) = interpn(unique(p.Y), unique(p.X), unique(p.Z), I, 0, p.x2, 0);
-            % max current value on the surface of the retina
-            amp_max(k, z, r) = max(max(I(:, :, round(size(I, 3)/2))));
-            % current value directly in between the two electrodes
-            amp_midpoint(k,z,r) = interpn(unique(p.Y), unique(p.X), unique(p.Z), I, 0, 0, 0);
+for z = 1:length(ret_s.z_range)
+    disp(['fitting z = ', num2str(z),  ' out of ', num2str(length(ret_s.z_range))]);
+    ret_s.z = -ret_s.z_range(z);
+    ret_s = cs.calc_dist_from_electrode(ret_s);
+    for a = 1:length(ret_s.a_range)
+        disp(a)
+        for k = 1:length(ret_s.k_range)
+            ret_s.k = ret_s.k_range(k);
+            ret_s.a = ret_s.a_range(a);
+            % find the threshold for the z
+            ret_s = cs.fit_currentspreadfast(ret_s, fitParams);
+            ret_s.t_fit(a, k, z) = ret_s.t;
         end
     end
 end
 
-%% Dip, Panel B
-dip_range = [30 70];
-thr_range = [300 500];
 
-dip = NaN(length(k_vals), length(z_vals));
-figure(2); clf
+%% plot thresholds as a function of height, where no retinal damage, single electrode
 
-for k = 1:length(k_vals)
-    dip(k, :)= 100.*(amp_max(k, :, 1)-amp_midpoint(k, :, 1))./amp_max(k, :, 1);
-    dip_intersect = interp1(dip(k, :), z_vals, [dip_range(1) min([max(dip(k,:)) dip_range(2)])]);
-    ind = z_vals<=dip_intersect(1) & z_vals>=dip_intersect(2);
-    plot(z_vals, dip(k,:), 'k'); hold on
-    plot(z_vals(ind), dip(k,ind), 'r'); hold on
-    patch([0 max(z_vals)  max(z_vals) 0 ], [dip_range(1) dip_range(1) dip_range(2) dip_range(2)] ,[.8 .8 .8],  'FaceColor',[ .8 .8 .8], 'EdgeColor', 'none', 'FaceAlpha',.3);
-end
-set(gca, 'YLim', [0 100]);
-xlabel('z (electrode-retina distance')
-ylabel(' 1 percept  <-   dip size  ->   2 percepts ')
+subplot(1,2,1)
+for a = 1:length(ret_s.a_range)
+    for k = 1:length(ret_s.k_range )
+        figure(1); 
+        subplot(1,2,1)
+        plot(ret_s.z_range, squeeze(ret_s.t_fit(a,k, :)), 'k'); hold on
+        xlabel('z'); ylabel('threshold non damaged retina')
+        
+        text(ret_s.z_range(end-1),  ret_s.t_fit(1,k, end-1), ['k = ',num2str(ret_s.k_range(k))]);
+        if k==3 & a==3
+              plot(ret_s.z_range, squeeze(ret_s.t_fit(a,k, :)), 'r'); hold on
+        end
+        set(gca, 'XLim', [0 1000])
+        set(gca, 'XTick',[0:200:1000])
+        set(gca, 'YLim', [0 700])
+        set(gca, 'YTick',[0:100:700])
 
-%% Intersection of constraint curves, Panel C
-figure(3); clf
-for k = 1:length(k_vals)
-    dip_intersect = interp1(dip(k, :), z_vals, [dip_range(1) min([max(dip(k,:)) dip_range(2)])]);
-    for r = 1:length(rd_vals)
-        goodvals(k, :, r)= z_vals<=dip_intersect(1) & z_vals>=dip_intersect(2) & amp(k,:,r)>=thr_range(1) & amp(k,:,r)<=thr_range(2);
-        plot(z_vals,  amp(k,:,r), 'k'); hold on
-        plot(z_vals(goodvals(k,:,r)),  amp(k, goodvals(k, :, r),r), 'r'); hold on
+        subplot(1,2,2)
+
+        plot(log(ret_s.z_range), squeeze(log(ret_s.t_fit(a,k, :))) , 'k'); hold on
+        if k==3 & a==3
+              plot(log(ret_s.z_range), log(squeeze(ret_s.t_fit(a,k, :))), 'r'); hold on
+        end
+        xlabel('z'); ylabel('threshold non damaged retina')
+        text(log(ret_s.z_range(end-1)),  log(ret_s.t_fit(1,k, end-1)), ['k = ',num2str(ret_s.k_range(k))]);
+
+        set(gca, 'XTick', log([ 100 1000]))
+        set(gca, 'XLim', log([50 2000]))
+        set(gca, 'XTickLabel',[100 1000])
+        logx2raw
+
+        set(gca, 'YTick', log([10  100 1000]))
+        set(gca, 'YLim', log([5 2000]))
+        set(gca, 'YTickLabel',[10 100 1000])
+        logy2raw
+      %  val(a, k) = input(' good = 1, bad = 0 ...' );
     end
 end
-xlabel('z (electrode-retina distance')
-ylabel('threshold (uAmps)')
-patch([0 max(z_vals)  max(z_vals) 0 ], [thr_range(1) thr_range(1) thr_range(2) thr_range(2)] ,[.8 .8 .8],  'FaceColor',[ .8 .8 .8], 'EdgeColor', 'none', 'FaceAlpha',.3);
+
+
+%% calculate surfaces for pair of electrodes, wide range of retinal damage
+ret_e = ret; % example electrode pair
+ret_e.k = 5; 
+ret_e.a = 2;
+ret_e.k = 12; 
+ret_e.t_lift = 0;
+ret_e.x = [-948 948];
+ret_e.y = [0 0];
+ret_e.t = [400];
+ret_e.t_ret = ret.t_ret_min;
+
+ret_p = ret_e; % save these parameters for the full simulation
+
+%% plot, current spreads for 2 electrodes at different heights
+
+% example electrode
+z_list = [-150 -750]
+for zz = 1:2
+    ret_e.z = z_list(zz);
+    figure(zz+1);clf
+    ret_e = cs.calc_dist_from_electrode(ret_e);
+    [ret_e,~] = fit('cs.fit_currentspread',ret_e, {'t'});
+    % now double it and calculate what the current spread looks like
+    ret_e.t =  ret.t_ret_min * 2;
+    ret_e = cs.create_currentspread(ret_e);
+    set(gcf, 'Position', plt.pos);
+    set(gcf, 'Name', ['height = ', num2str(ret_e.z)]);
+    Sxy = ret_e.I(:, :, round(size(ret_e.I, 3)/2));
+    ret_e.amp_max = max(Sxy(:));
+    [y, x] = find(Sxy== ret_e.amp_max);
+    ret_e.amp_mid = interpn(unique(ret_e.Y), unique(ret_e.X),Sxy, 0, 0);
+    ret_e.dip= 100.*(ret_e.amp_max-ret_e.amp_mid)./ret_e.amp_max;
+    ret_e.loc = [x y];
+    cs.create_currentspreadfig(ret_e); drawnow
+end
+
+
+
+%% calculate for a range of paramter values
+ret_p.t_ret = ret_p.t_ret_min;
+for z = 1:length(ret_p.z_range)
+    disp(['fitting z = ', num2str(z),  ' out of ', num2str(length(ret_p.z_range))]);
+    ret_p.z = -ret_p.z_range(z);
+    ret_p = cs.calc_dist_from_electrode(ret_p);
+    for k = 1:length(ret_p.k_range)
+        for a = 1:length(ret_p.a_range)
+            if good_ak(a,k)==1
+            disp(['fitting k = ', num2str(k),  ' out of ', num2str(length(ret_p.k_range))]);
+            ret_p.k = ret_p.k_range(k);
+            for r = 1:length(ret_p.t_rd_range)
+                ret_p.t_ret= ret_p.t_rd_range(r) + ret_p.t_ret_min;  % assuming that on the retina more current is needed
+
+                % find the amplitude to reach this threshold
+                ret_p.t = 400;
+                ret_p = cs.fit_currentspreadfast(ret_p, fitParams);
+                ret_p.t_fit(a, k,z,r) = ret_p.t;
+                % now double it and calculate what the current spread looks like
+                ret_p.t = ret_p.t*2;
+                ret_p = cs.create_currentspread(ret_p);
+
+                % max current value on the surface of the retina
+                ret_p.amp_max(a, k,z,r) = max(max(ret_p.I(:, :, round(size(ret_p.I, 3)/2))));
+                % current value directly in between the two electrodes
+                ret_p.amp_mid(a, k,z,r) = interpn(unique(ret_p.Y), unique(ret_p.X), unique(ret_p.Z), ret_p.I, 0, 0, 0);
+            end
+            end
+        end
+    end
+end
+
+
+%% dipsize as a function of z and k, for a fixed value of r
+
+ret_p.dip= 100.*(ret_p.amp_max-ret_p.amp_mid)./ret_p.amp_max; % 100 means big dip
+for a = 1:length(ret_p.a_range)
+figure(5); subplot(2, 4, a)
+imagesc(ret_p.z_range, ret_p.k_range, squeeze(2.55*ret_p.dip(a, :,:, 1))); 
+colormap(gray(256)); hold on
+set(gca, 'XTick',ret_p.z_range); xlabel('z');
+set(gca, 'YTick',ret_p.k_range); ylabel('k');
+ylabel('k'); 
+title('dipsize');
+%c = colorbar('Ticks',linspace(1, 255, 5), 'TickLabels',{'0','25','50','75','100'});
+contour(ret_p.z_range, ret_p.k_range, squeeze(ret_p.dip(a, :,:, 1)), [30 70], 'g')
+end
+return
+
+%% intersection of constraints, which combinations of z and k, for a fixed r, are plausible
+
+
+for k = 1:length(ret_p.k_range)
+    tmin = ret_p.t_fit(k,1,1);
+    for z = 1:length(ret_p.z_range)
+        for r = 1:length(ret_p.t_rd_range)
+            ret_p.t_rd_fit(k,z,r) = ret_p.t_fit(k,z,r)-ret_p.t_fit(k,z,1);
+            ret_p.t_z_fit(k,z,r) = ret_p.t_fit(k,z,r)-(ret_p.t_rd_fit(k,z,r)+tmin); % what extra current did the lift require
+            round(ret_p.t_fit(k,z,r))
+            round(tmin+ret_p.t_rd_fit(k,z,r) + ret_p.t_z_fit(k,z,r) )
+        end
+    end
+end
+
+%% dip doesn't change with retinal damage
+dip_range = [30 70];
+thr_range = [177 597];
+cmap = [0 0 0 ; 0 0 1 ; 1 0 0 ;  1 1 1 ];
+r=3;
+ret_p.gv_thr = zeros(size(ret_p.amp_max));ret_p.gv_dip = ret_p.gv_thr;
+
+ret_p.gv_thr(find(ret_p.t_fit(:)>thr_range(1) & ret_p.t_fit(:)<thr_range(2)))=2;
+ret_p.gv_dip(find(ret_p.dip(:)>dip_range(1) & ret_p.dip(:)<dip_range(2)))=1;
+ret_p.gv = (ret_p.gv_thr+ret_p.gv_dip)==3;
+
+figure(5); clf
+image(ret_p.z_range, ret_p.k_range, 1+ret_p.gv_dip(:,:, r)+ret_p.gv_thr(:,:, r)); colormap(cmap); hold on
+set(gca, 'XTick',ret_p.z_range); xlabel('z');
+set(gca, 'YTick',ret_p.k_range); ylabel('k');
+ylabel('k'); 
+title(['intersection ret damage  = ', num2str(ret_p.t_rd_range(r))]);
+c = colorbar('Ticks',[1 85 169 255], 'TickLabels',{'neither','dip','thr','dip+thr'});
+
 
 %% patch of good values, Panel D
-cmap = hot(length(k_vals)+8);
-rd_amp  = amp-amp(:,:,1);
-z_amp = amp(:,:,1)-min(amp(:));
-z_amp = repmat(z_amp,[1 1  size(rd_amp,3)]);
+cmap = hot(length(ret_p.k_range)+8);
+figure(6); clf
 
-figure(4); clf
+for k=length(ret_p.k_range):-1:1
+    gv =ret_p.gv(k,:,:); gv_idx = find(gv(:)); % find the values that are ok
+    t_z = ret_p.t_z_fit(k, :, :); t_z = t_z(:); t_z = t_z(gv_idx); % collect the increase in thr due to lift
+    t_all = ret_p.t_fit(k, :, :);t_all = t_all(:); t_all = t_all(gv_idx);
+    t_rd = ret_p.t_rd_fit(k, :, :); t_rd = t_rd(:); t_rd = t_rd(gv_idx); 
+    zv = repmat(ret_p.z_range, 1, length(ret_p.t_rd_range)); zv = zv(:); zv = zv(gv_idx);
+    round(min(t_z(:)+t_rd(:)))
+    round(max(t_z(:)+t_rd(:)))
+    round(max(t_all(:)))
 
-for k=length(k_vals):-1:1
-    gv =goodvals(k,:,:);gvi = find(gv(:));
-    xv = rd_amp(k, :, :);  xv = xv(:); xg = xv(gvi);
-    yv = z_amp(k,:, :); yv = yv(:); yg = yv(gvi);
-
-    if length(xg)>3
-        DT = delaunayTriangulation(xg,yg);
+    if length(gv_idx)>6
+        DT = delaunayTriangulation(t_z,t_rd);
         C = convexHull(DT);
-        h(k)=patch(DT.Points(C,1),DT.Points(C,2), cmap(k,:), 'EdgeColor', 'none', 'FaceAlpha',.3);
+        h(k)=patch(DT.Points(C,1),DT.Points(C,2), cmap(k,:), 'EdgeColor', 'none', 'FaceAlpha',.3); hold on
     end
 end
+set(gca, 'XLim', [ 0 300])
+set(gca, 'YLim', [ 0 600])
 xlabel('threshold elevation due to retinal degeneration')
 ylabel('threshold elevation due to array lift')
+
+
+%% thresholds
+tdata = {1	'B10'	89.0; 1	'B10'	93.0; 2	'A8'	153; 1	'B9'	177; ...
+1	'F10'	177; 2	'A4'	194; 1	'F10'	202; 1	'C6'	210; 2	'F2'	226; ...
+1	'E9'	242; 1	'A8'	250; 2	'D1'	274; 2	'F2'	274; 2	'F7'	274; ...
+1	'A6'	290; 1	'D6'	290; 1	'B4'	323; 2	'B6'	323; 2	'A2'	355; ...
+2	'E10'	484; 3	'B6'	581.0; 3	'F7'	612.5; 3	'B9'	645.0; 3	'B5'	645.0; ...
+3	'A10'	371.0; 3	'F7'	452.0; 3	'F9'	475.5; 3	'B9'	306.0; 3	'B10'	217.0};
+cmap = [.3 .3 .3; .5 .5 .5; .7 .7 .7];
+
+for s = 1:3
+idx = find([tdata{:, 1}]== s);
+ counts(s,:) = histc(cat(1, tdata{idx, 3}), [0:100:700]);
+end
+
+figure(10); clf
+b = bar([0:100:700],counts,'stacked');
+for s = 1:3
+    set(b(s), 'FaceColor', cmap(s, :));
+end
+
+val = []
+for s = [1 3]
+idx = find([tdata{:, 1}]== s);
+ val = cat(1, val,cat(1, tdata{idx, 3}) );
+end
+round(prctile(val, [15 85]))
+
+
+
 
 
 
